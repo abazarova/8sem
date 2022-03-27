4dfa9e7d7e39b141e52a596bdc8ab5e38c878cdf
abazarova <you@example.com>
2022-03-27 12:48:23 +0300

    add system solving

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ typedef Eigen::Triplet<double> T;
 
 int main(int argc, char** argv){
 
+	//read data from file
 	if(argc!=2){
 		std::cerr << "Error: expected one and only one argument.\n";
 		return -1;
@@ -39,11 +40,42 @@ int main(int argc, char** argv){
 		fscanf(fs, "%lf", &A[i]);
 	}
 
-
+	// convert data into Eigen SparseMatrix format
 	vector<T> tripletList;
 	tripletList.reserve(n);
+	int num_elems_row = 0, k = 0;
+	double v = 0;
 	for (i = 0; i < n; i++){
 		// i - current row number
-		num_elems_row = 
+		num_elems_row = IA[i + 1] - IA[i];
+		for (k = 0; k < num_elems_row; k++){
+			j = JA[IA[i] + k];
+			v = A[IA[i] + k];
+			tripletList.push_back(T(i, j, v));
+		}
 	}
-}
+	SparseMatrix<double> M(n, n);
+	M.setFromTriplets(tripletList.begin(), tripletList.end());
+	
+
+	// solve the system
+	VectorXd b(n), x(n);
+	for (i = 0; i < n; i++){
+		b[i] = 0;
+		x[i] = -1;
+	}
+	SparseLU<SparseMatrix<double>, COLAMDOrdering<int> > solver;
+	//initialization: columns reordering and A factorization
+	solver.analyzePattern(M); 
+	// Compute the numerical factorization 
+	solver.factorize(M); 
+	// Use the factors to solve the linear system 
+	x = solver.solve(b);
+	cout << x[0] << endl;
+	return 0;
+
+
+
+
+
+}	
